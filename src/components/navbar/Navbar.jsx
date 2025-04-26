2ecc89d69605c81842bff3ca120f424c447068a8
wchandler2020 <william.d.chandler1@gmail.com>
2025-04-26 12:41:10 -0500

    fixed: add links to social sites in the  navboar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import myLogo from '../../assets/williamchandlerlogo.png'
-import {FaLinkedin} from 'react-icons/fa'
-import {FaGithub} from 'react-icons/fa'
-import {FaSquareXTwitter} from 'react-icons/fa6'
-import {FaInstagram} from 'react-icons/fa'
+import React from "react";
+import myLogo from "../../assets/williamchandlerlogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-        <div className='flex flex-shrink-0 items-center'>
-            <img src={myLogo} alt='logo' className='mx-2 w-20' />
-        </div>
-        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-            <FaLinkedin />
-            <FaGithub />
-            <FaSquareXTwitter />
-            <FaInstagram />
-        </div>
+      <div className="flex flex-shrink-0 items-center">
+        <img src={myLogo} alt="logo" className="mx-2 w-20" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/william-d-chandler/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/wchandler2020">
+          <FaGithub />
+        </a>
+        <a href="#">
+          {" "}
+          <FaSquareXTwitter />
+        </a>
+        <a href="#">
+          {" "}
+          <FaInstagram />
+        </a>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
